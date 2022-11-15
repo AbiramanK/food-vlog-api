1b42227aa8864cee3223df5a551a7e009f5a0d12
Abiraman K <abiramancit@gmail.com>
2022-11-15 15:48:36 +0530

    [Abiraman K] Adds db connection & migration setup

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ dotenv.config();
 import "reflect-metadata";
 import { ApolloServer } from "apollo-server";
 import { buildSchema } from "type-graphql";
+import { sequelize } from "./dbconfig";
 
 async function startServer() {
   const schema = await buildSchema({
@@ -12,6 +13,13 @@ async function startServer() {
   const server = new ApolloServer({
     schema,
   });
+
+  try {
+    await sequelize.authenticate();
+    console.log("✔✔ Databse Connection has been established successfully.");
+  } catch (error) {
+    console.error("❌❌ Database Unable to connect to the database:", error);
+  }
 
   const PORT = parseInt(process.env.NODE_APP_SERVER_PORT!) ?? 8000;
 
