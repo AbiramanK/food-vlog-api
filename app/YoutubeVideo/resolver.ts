eb20f96aa6226b6da6569910ef8bc2decad4d7a8
Abiraman K <abiramancit@gmail.com>
2022-11-16 14:41:39 +0530

    [Abiraman K] Adds query get_latest_youtube_videos with pagination

--- app/YoutubeVideo/resolver.ts
+++ app/YoutubeVideo/resolver.ts
@@ -1,4 +1,60 @@
-import { Resolver } from "type-graphql";
+import {
+  Arg,
+  Field,
+  InputType,
+  ObjectType,
+  Query,
+  Resolver,
+} from "type-graphql";
+import { YoutuebVideoModel } from "./model";
+
+@InputType()
+class PaginationInput {
+  @Field()
+  limit: number = 10;
+
+  @Field()
+  cursor: number = 0;
+}
+
+@ObjectType()
+class PageInfo {
+  @Field()
+  declare hasMore: boolean;
+
+  @Field()
+  declare cursor: number;
+}
+
+@ObjectType()
+class PaginationOutput {
+  @Field(() => [YoutuebVideoModel])
+  declare edgeds: YoutuebVideoModel[];
+
+  @Field()
+  declare pageInfo: PageInfo;
+}
 
 @Resolver()
-class YoutubeVideoResolver {}
+export class YoutubeVideoResolver {
+  @Query(() => PaginationOutput)
+  async get_latest_youtube_videos(
+    @Arg("input") input: PaginationInput
+  ): Promise<PaginationOutput> {
+    const youtubeVideos = await YoutuebVideoModel.findAndCountAll({
+      order: [["published_at", "desc"]],
+      limit: input.limit,
+      offset: input.cursor,
+    });
+
+    const cursor = youtubeVideos?.rows?.length!;
+    const hasMore = cursor < youtubeVideos?.count;
+    return {
+      edgeds: youtubeVideos?.rows!,
+      pageInfo: {
+        cursor,
+        hasMore,
+      },
+    };
+  }
+}
